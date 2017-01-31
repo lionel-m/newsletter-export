5f2ad749ec921c69e8f233253e56950a7dfc6e6b
lionel-m <work@lionelmaccaud.ch>
2017-01-31 22:14:06 +0100

    Improvements
    The master branch is for Contao 4

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
-## Newsletter export bundle for the [Contao CMS](https://contao.org/en)
+## Newsletter export extension for the Contao CMS
+
+Contao repository : https://contao.org/en/extension-list/view/newsletter_export.html
 
 Originally created by John Brand (http://www.brandbrilliance.co.za).
 
 License : LGPL-3.0
 
-### Installation
+### Contao 4.x beta
 
-Add `"lionel/newsletter-export": "4.0.*"` in the `composer.json` file.
+Add `"lionel/newsletter-export": "~4.0@beta"` in the `composer.json` file.
 
 Add `new LionelM\NewsletterExportBundle\LionelMNewsletterExportBundle(),` in the `app/AppKernel.php` file.
 
-Create the symlink in `web/bundles` with `php app/console assets:install web --symlink`.
+Run the command `php composer.phar update newsletter-export`.
+
+If needed, create the symlink in `web/bundles` with `php app/console assets:install web --symlink`.
--- src/Resources/contao/classes/NewsletterExport.php
+++ src/Resources/contao/classes/NewsletterExport.php
@@ -16,51 +16,73 @@
  */
 namespace LionelM\NewsletterExportBundle;
 
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * Class NewsletterExport
  *
  * Provide methods to handle subscriber export.
  * @author  John Brand
+ * @author  Lionel Maccaud
+ * @author  fritzmg
  * @package Controller
  */
 class NewsletterExport extends \Backend
 {
-    /**
+  /**
      * Return a form to choose a CSV file and import it
+     * Resource: http://www.andrew-kirkpatrick.com/2013/08/output-csv-straight-to-browser-using-php/
      * @param object
      * @return string
      */
-    public function exportRecipients(\DataContainer $dc)
-    {
-        if ($this->Input->get('key') != 'export') {
-            return '';
-        }
-
-        // get records
-        $query = "SELECT m.firstname, m.lastname, m.gender, r.email, r.active
-                    FROM tl_newsletter_recipients AS r
-                    LEFT JOIN tl_member AS m ON m.email = r.email
-                   WHERE r.pid = ?
-                ORDER BY r.email ASC";
-        $objRow = $this->Database->prepare($query)->execute($dc->id);
-
-        // start output
-        $exportFile = 'newsletter_recipients_export_' . date("Ymd-Hi");
-
-        header('Content-Type: application/csv');
-        header('Content-Transfer-Encoding: binary');
-        header('Content-Disposition: attachment; filename="' . $exportFile .'.csv"');
-        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-        header('Pragma: public');
-        header('Expires: 0');
-
-        $output = '"Firstname","Lastname","Gender","E-mail","Active"'. "\n";
-
-        while ($objRow->next()) {
-            $output .= '"' . join('","', $objRow->row()).'"' . "\n";
-        }
-
-        echo $output;
-        exit;
+  public function exportRecipients(\DataContainer $dc)
+  {
+    if ($this->Input->get('key') != 'export') {
+      return '';
     }
+
+    // Headings and rows
+    $headings = array('Firstname', 'Lastname', 'Gender', 'E-mail', 'Active');
+
+    // Open the output stream
+    $handle = fopen('php://output', 'w');
+
+    // Start output buffering (to capture stream contents)
+    ob_start();
+
+    fputcsv($handle, $headings, ';');
+
+    // get records
+    $query = "SELECT m.firstname, m.lastname, m.gender, r.email, r.active
+              FROM tl_newsletter_recipients AS r
+              LEFT JOIN tl_member AS m ON m.email = r.email
+              WHERE r.pid = ?
+              ORDER BY r.email ASC";
+
+    $objRow = $this->Database->prepare($query)->execute($dc->id);
+
+    // Add the data queried from database
+    while ($objRow->next())
+    {
+      fputcsv($handle, array($objRow->firstname, $objRow->lastname, $objRow->gender, $objRow->email, $objRow->active), ';');
+    }
+
+    // Get the contents of the output buffer
+    $string = ob_get_clean();
+
+    $filename = 'newsletter_recipients_export_' . date("Y-m-d_Hi");
+
+    $response = new Response();
+
+    $response->headers->set('Content-Type', 'text/csv; charset=utf-8');
+    $response->headers->set('Content-Transfer-Encoding', 'binary');
+    $response->headers->set('Content-Disposition', 'attachment; filename="' . $filename .'.csv"');
+    $response->headers->set('Cache-Control', 'must-revalidate, post-check=0, pre-check=0');
+    $response->headers->set('Pragma', 'public');
+    $response->headers->set('Expires', '0');
+
+    $response->send();
+
+    exit($string);
+  }
 }
